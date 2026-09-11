9540894b1b38dc21ac98b085b2a605ad8057c1a7
Xkatrice <khlbarbour@gmail.com>
2026-09-11 12:34:13 +0100

    overhauled css and look of app with icons

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ calculateButton.addEventListener("click", calculateScore);
 
 function calculateScore() {
 
+// Tasks Scores 
 
     const forestInput = document.getElementById("forest_tasks");
     const forestPoints = Number(forestInput.value);
@@ -20,15 +21,26 @@ function calculateScore() {
     const riverInput = document.getElementById("river_tasks");
     const riverPoints = Number(riverInput.value);
 
+ // Long river and rail scores 
+
     const longRailInput = document.getElementById("long_rail");
     const longRailPoints = Number(longRailInput.value);
 
     const longRiverInput = document.getElementById("long_river");
     const longRiverPoints = Number(longRiverInput.value);
 
-    const flagsInput = document.getElementById("flags_points");
-    const flagsPoints = Number(flagsInput.value);
+// Flags scoring
 
+    const forestFlagInput = document.getElementById("flag_forest");
+    const forestflagsPoints = Number(forestFlagInput.value);
+
+    const grainFlagInput = document.getElementById("flag_grain");
+    const grainFlagsPoints = Number(grainFlagInput.value);
+
+    const villageFlagInput = document.getElementById("flag_village");
+    const villageFlagsPoints = Number(villageFlagInput.value);
+
+// Unlocked Scoring
 
     const heartsInput = document.getElementById("heart_points");
     const heartsPoints = Number(heartsInput.value);
@@ -44,18 +56,54 @@ function calculateScore() {
         circusPoints = 10;
     }
 
+// subtotals 
+
     const taskTotal = forestPoints + grainPoints + villagePoints + railPoints + riverPoints
 
-    const totalPoints = taskTotal + longRailPoints + longRiverPoints + flagsPoints + heartsPoints + harvestPoints + circusPoints;
+    const flagTotal = forestflagsPoints + grainFlagsPoints + villageFlagsPoints
 
+    const longTotal = longRailPoints + longRiverPoints
+
+    const specialPoints = heartsPoints + harvestPoints + circusPoints
+
+    const totalPoints = taskTotal + longTotal + flagTotal + specialPoints;
+
+// display subtotals
 
     const taskScoreDisplay = document.getElementById("task_points");
-
     taskScoreDisplay.textContent = taskTotal;
+     
+    const summaryTaskScoreDisplay = document.getElementById("summary_task_points");
+    summaryTaskScoreDisplay.textContent = taskTotal;
+
+    const specialScoreDisplay = document.getElementById("special_points")
+    specialScoreDisplay.textContent = specialPoints
 
     const scoreDisplay = document.getElementById("total_points");
-
     scoreDisplay.textContent = totalPoints;
+
+    const flagScoreDisplay = document.getElementById("flag_points");
+    flagScoreDisplay.textContent = flagTotal;
+
+    const longScoreDisplay = document.getElementById("longest_points");
+    longScoreDisplay.textContent = longTotal;
+
+    const longRiverScoreDisplay = document.getElementById("points_long_river");
+    longRiverScoreDisplay.textContent = longRiverPoints;
+
+    const longRailScoreDisplay = document.getElementById("points_long_rail");
+    longRailScoreDisplay.textContent = longRailPoints;
+
+    const summaryFlagScoreDisplay = document.getElementById("summary_flag_points");
+    summaryFlagScoreDisplay.textContent = flagTotal;
+
+    const summarySpecialScoreDisplay = document.getElementById("summary_special_points");
+    summarySpecialScoreDisplay.textContent = specialPoints;
+
+
+
+
+
 
   console.log(circusPoints);
 }
